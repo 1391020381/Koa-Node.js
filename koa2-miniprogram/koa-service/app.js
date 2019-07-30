5fe66637ec8926c27d82553fdae1c5549d53de21
zhoujin <zhoujin@iboxpay.com>
2019-07-30 16:54:01 +0800

    添加log4js

--- koa2-miniprogram/koa-service/app.js
+++ koa2-miniprogram/koa-service/app.js
@@ -4,13 +4,27 @@ const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 const koaStatic = require('koa-static')
 const router = require('./router')
-const { open ,error} = require('./lib/db/connect')
-app.use(koaStatic(path.join(__dirname,'uploads')))
+const { open, error } = require('./lib/db/connect')
+app.use(koaStatic(path.join(__dirname, 'uploads')))
+const logger = require('./middlewares/log4.js')
+app.use(logger)
 app.use(bodyParser())
- app.use(router.routes())
+app.use(async (ctx, next) => { // 统一错误处理
+  try {
+    await next()
+  } catch (e) {
+    ctx.logger.error(e.stack || e)
+    ctx.body = {
+      status:-1,
+      message:e.message || e,
+      code:e.status
+    }
+  }
+})
+app.use(router.routes())
 app.use(router.allowedMethods())
 open()
 error()
-app.listen(4001,()=>{
+app.listen(4001, () => {
   console.log('app is listening 4001,http://127.0.0.1:4001')
 })
